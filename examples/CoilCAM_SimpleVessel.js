591a40f3f7475806e3aa44e79921ae3c8fb8d231
sam bourgault <samuelle.bourgault@gmail.com>
2024-04-12 14:35:44 -0600

    small cahange on example vessel

--- examples/CoilCAM_SimpleVessel.js
+++ examples/CoilCAM_SimpleVessel.js
@@ -9,9 +9,9 @@ var potterbot_bedSize = [280, 265, 305];
 //--Main Vessel
 var nbLayers = 60;
 var nbPointsInLayer = 30;
-var ssp = sinusoidal(20, 30, 0, nbLayers, 0, "");
-//var ssp = sinusoidal2D(9.399, 50.0, 0.0, 50.0, 40, 12.5, [], "");
-//var ssp = sinusoidal2D(-0.251, 48, 0.0, 0.0, nbLayers, ssp, "");
+//var rsp = sinusoidal(5, 5, 0, nbPointsInLayer, 0, "");
+//var ssp0 = linear(0.5,0,nbLayers,0,"");
+var ssp = sinusoidal(10, 15, 0, nbLayers, 0, "");
 var toolpath_main = toolpathUnitGenerator([0.0, 0.0, 4.5], 42.00, potterbot_layerHeight, nbLayers, nbPointsInLayer, [], ssp, [], [], []);
 
 updatePath(toolpath_main);
