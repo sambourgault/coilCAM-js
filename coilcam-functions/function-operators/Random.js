78246bf16d33b952d90381dabdc3b706d94ab8e9
Esme <esmepuzio@ucsb.edu>
2024-06-25 17:23:51 -0700

    Random bumps V1 added

--- coilcam-functions/function-operators/Random.js
+++ coilcam-functions/function-operators/Random.js
@@ -1,10 +1,11 @@
-function append(heightRange, widthRange, scatterRange, nbPoints, nbBumps, mode){
+function append(heightRange, widthRange, scatterRange, nbPoints, offset, mode){
     if(mode == "square"){
         while (true){
             let height = heightRange[0];
             let width = widthRange[0];
             let scatter = scatterRange[0];
             let triangularScalingFactor = 2*height/width;
+            let sawtoothScalingFactor = height/width;
             if(heightRange.length == 2){
                 height = Math.random(heightRange[1] - heightRange[0]) + heightRange[0];
             }
@@ -16,29 +17,31 @@ function append(heightRange, widthRange, scatterRange, nbPoints, nbBumps, mode){
             }
             for (let i = 0; i < width; i++){ 
                 if (values.length >= nbPoints){
-                    return values;
+                    return values; //break
                 }
                 if(mode == "triangle"){
                     if(i%width < i/2){
-                        values.push(((i%width)*triangularScalingFactor));
+                        values.push(((i%width)*triangularScalingFactor) + offset[i]);
                     }
                     else if(i%width > i/2){
-                        values.push((height-(i%Math.ceil(width/2))*triangularScalingFactor));
+                        values.push((height-(i%Math.ceil(width/2))*triangularScalingFactor) + offset[i]);
                     } else{
-                        values.push(height);
+                        values.push(height + offset[i]);
                     }
                 }
                 if(mode == "square"){
-                    values.push(height);
+                    values.push(height + offset[i]);
                 }
                 if(mode == "circle"){ //y = sqrt((r^2) - ((x/h)-r)^2)) r= height, h=width
                     if(height < 0){ //height is negative
-                        values.push(-Math.sqrt((-height/2)**2 - ((x/width)+height)));
+                        values.push(-Math.sqrt((-height/2)**2 - ((x/width)+height)) + offset[i]);
                     } else{
-                        values.push(Math.sqrt((height/2)**2 - ((x/width)-height)));
+                        values.push(Math.sqrt((height/2)**2 - ((x/width)-height)) + offset[i]);
                     }
                 }
-                
+                if(mode == "sawtooth"){ 
+                    values.push(((i%width)*sawtoothScalingFactor) + offset[i]);
+                }
             }
             for (let i = 0; i < scatter; i++){ 
                 if (values.length >= nbPoints){
@@ -51,12 +54,25 @@ function append(heightRange, widthRange, scatterRange, nbPoints, nbBumps, mode){
 }
 
 
-function randomBumps(heightRange, widthRange, scatterRange, nbPoints, nbBumps, mode){
+function randomBumps(heightRange, widthRange, scatterRange, nbPoints, offset, mode){
+    if(offset.length === 0){
+        offset = new Array(nbPoints).fill(0);
+    } else if(!Array.isArray(offset)){
+        offset = new Array(nbPoints).fill(offset);
+    } else if(offset.length !== nbPoints){
+        throw new Error("Length of offset in Square must be 0 or 1 or equal to nbPoints");
+    }
+    
     if(widthRange[0] < 0 || (widthRange.length == 2 && widthRange[1] < 0)){
         throw new Error("Width range is negative.");
     }
     if(scatterRange[0] < 0 || (scatterRange.length == 2 && scatterRange[1] < 0)){
         throw new Error("Scatter range is negative.");
     }
-    return append(heightRange, widthRange, scatterRange, nbPoints, nbBumps, mode);
+    let modes = ["square", "circle", "triangle", "sawtooth"];
+    if(mode !== modes){
+        mode = "square";
+    }
+    
+    return append(heightRange, widthRange, scatterRange, nbPoints, offset, mode);
 }
