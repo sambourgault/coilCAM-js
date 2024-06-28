082d2c2d7d2fc922a6e6fab1800962363a1c584f
Esme <esmepuzio@ucsb.edu>
2024-06-28 10:07:02 -0700

    Waveform first pass

--- coilcam-functions/function-operators/Waveform.js
+++ coilcam-functions/function-operators/Waveform.js
@@ -8,36 +8,22 @@ function waveform(filename, nbPoints='default', offset=0, heightRange=[-1,1]){
 
     let binaryString = atob(wavFile.split('base64,')[1]); //remove metadata
     if(nbPoints == 'default'){ //nbPoints = how many points to return/samples for waveform
-        binaryString.length = nbPoints;
+       nbPoints = binaryString.length;
     }
     let bytes = new Uint8Array(nbPoints);
-    console.log(binaryString.length);
-    console.log(Math.floor(binaryString.length*(1/nbPoints)));
+    // console.log(binaryString.length);
+    // console.log(Math.floor(binaryString.length*(1/nbPoints)));
     for(let i = 0; i < binaryString.length; i++){
         if(i % Math.floor(binaryString.length/nbPoints) == 0){ //push back nbPoints points to new array
             bytes[i] = binaryString.charCodeAt(i);        
         }
     }
-    console.log(bytes);
-    // let audioContext = new window.AudioContext;
-    // audioContext.decodeAudioData(bytes.buffer, (audioBuffer) => {
-    //     const pcmData = audioBuffer.getChannelData(0); //mono audio
-    //     console.log(pcmData);
-        // const fftSize = 2048; // Define the size of the FFT
-        // const analyser = audioContext.createAnalyser();
-        // analyser.fftSize = fftSize;
-        // const frequencyData = new Float32Array(analyser.frequencyBinCount);
-
-        // // Connect the buffer source to the analyser
-        // const source = audioContext.createBufferSource();
-        // source.buffer = audioBuffer;
-        // source.connect(analyser);
-        // source.start();
-
-        // // Get the frequency data
-        // analyser.getFloatFrequencyData(frequencyData);
-        // console.log(frequencyData);
-    // })
+    let nums = [];
+    for(let i = 0; i < bytes.length; i++){
+        nums.push(bytes[i] / (256*heightRange[1]) + heightRange[0]);
+    }
+    return nums;
+    // return bytes;
 }
    
 
